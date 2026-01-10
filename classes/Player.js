0eb2bd124ab3ef417417c68063392a4f0901f301
DMDstd <domedastudios@gmail.com>
2026-01-10 21:07:20 +0100

    e

--- classes/Player.js
+++ classes/Player.js
@@ -40,7 +40,7 @@ move(d, walls, water, enemies) {
     startInteraction(result.entity, player);
   }
 }
-jump(ee,walls, water, enemies) {
+jump(walls, water, enemies) {
   let nx = this.x;
   let ny = this.y;
   let result = this.collides(nx, ny, walls, water, enemies, entities);
